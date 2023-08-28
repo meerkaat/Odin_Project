b900f551c41b916c33bc347d566e308364365ae8
meerkaat <58065364+meerkaat@users.noreply.github.com>
2023-08-27 23:57:51 -0500

    reorganized

--- rps/dist/main.js
+++ rps/dist/main.js
@@ -6,76 +6,80 @@ function getElementByIdOrThrow(selector, msg = "Element not found") {
         throw new Error(msg);
     return element;
 }
-function main() {
-    const resultElm = getElementByIdOrThrow("result");
-    const comChoice = getElementByIdOrThrow("com-choice");
-    const round1 = getElementByIdOrThrow("round1");
-    const round2 = getElementByIdOrThrow("round2");
-    const round3 = getElementByIdOrThrow("round3");
-    const tieBreaker = getElementByIdOrThrow("tie-breaker");
-    const match = getElementByIdOrThrow("match-result");
-    const resetbtn = getElementByIdOrThrow("reset");
-    resetbtn.disabled = true;
-    const btns = function () {
-        let buttons = [];
-        for (const prs of choices) {
-            buttons.push(getElementByIdOrThrow(`btn-${prs}`));
-        }
-        return buttons;
+const comChoice = getElementByIdOrThrow("com-choice");
+const round1 = getElementByIdOrThrow("round1");
+const round2 = getElementByIdOrThrow("round2");
+const round3 = getElementByIdOrThrow("round3");
+const tieBreaker = getElementByIdOrThrow("tie-breaker");
+const match = getElementByIdOrThrow("match-result");
+const resetbtn = getElementByIdOrThrow("reset");
+resetbtn.disabled = true;
+resetbtn.addEventListener("click", () => {
+    location.reload();
+});
+function btns() {
+    let buttons = [];
+    for (const prs of choices) {
+        buttons.push(getElementByIdOrThrow(`btn-${prs}`));
+    }
+    return buttons;
+}
+;
+const btnsArr = btns();
+function disableBtns(buttons) {
+    for (const btn of buttons) {
+        btn.disabled = true;
+    }
+}
+let counter = 1;
+let roundResultsArr = [];
+function displayRoundResults(verdict) {
+    const elements = {
+        1: round1,
+        2: round2,
+        3: round3,
+        4: tieBreaker,
     };
-    function disableBtns() {
-        for (const btn of btns()) {
-            btn.disabled = true;
+    if (counter in elements) {
+        const element = elements[counter];
+        let result = emojiMapping[verdict];
+        if (counter <= 3) {
+            element.textContent = `Round ${counter}: ${result}`;
+            roundResultsArr.push(result);
+        }
+        else {
+            tieBreaker.textContent = `Tie Breaker: ${result}`;
+            roundResultsArr.push(result);
         }
     }
-    let counter = 1;
-    let roundResultsArr = [];
-    function displayRoundResults(verdict, uc, cc) {
-        const elements = {
-            1: round1,
-            2: round2,
-            3: round3,
-            4: tieBreaker,
-        };
-        if (counter in elements) {
-            const element = elements[counter];
-            let result = emojiMapping[verdict];
-            if (counter <= 3) {
-                element.textContent = `Round ${counter}: ${result}`;
-                roundResultsArr.push(result);
-            }
-            else {
-                tieBreaker.textContent = `Tie Breaker: ${result}`;
-                roundResultsArr.push(result);
-            }
-        }
-        counter++;
+    counter++;
+}
+function evaluateOverallWinner() {
+    const counts = new Map();
+    for (const value of roundResultsArr) {
+        counts.set(value, (counts.get(value) ?? 0) + 1);
+        counts.delete(emojiMapping.Tie);
     }
-    function evaluateOverallWinner() {
-        const counts = new Map();
-        for (const value of roundResultsArr) {
-            counts.set(value, (counts.get(value) ?? 0) + 1);
-            counts.delete(emojiMapping.Tie);
-        }
-        for (const [emoji, count] of counts.entries()) {
-            if (count > 1) {
-                match.textContent = `Match: ${emoji}`;
-                disableBtns();
-                resetbtn.disabled = false;
-                break;
-            }
+    for (const [emoji, count] of counts.entries()) {
+        if (count > 1) {
+            match.textContent = `Match: ${emoji}`;
+            disableBtns(btnsArr);
+            resetbtn.disabled = false;
+            break;
         }
     }
-    function forceNoTie(cc) {
-        let tempChoices = choices.filter((value) => value !== cc);
-        let newCC = getRandomElement(tempChoices);
-        return newCC;
-    }
+}
+function forceNoTie(cc) {
+    let tempChoices = choices.filter((value) => value !== cc);
+    let newCC = getRandomElement(tempChoices);
+    return newCC;
+}
+function main() {
     let tieArr = [];
     btns().forEach((btn) => {
         btn.addEventListener("click", (ev) => {
             console.log("Second", tieArr);
-            const uc = getComputerChoice();
+            const uc = btn.getAttribute("data-choice");
             let cc = getComputerChoice();
             assert(typeof uc === "string" && isValidChoice(uc), "Not a valid choice");
             let verdict = evaluateGame(uc, cc);
@@ -86,14 +90,10 @@ function main() {
                     verdict = evaluateGame(uc, cc);
                 }
             }
-            displayRoundResults(verdict, uc, cc);
+            displayRoundResults(verdict);
             evaluateOverallWinner();
             comChoice.textContent = emojiMapping[cc];
-            resultElm.textContent = `User: ${emojiMapping[verdict]}`;
         });
-    });
-    resetbtn.addEventListener("click", (btn) => {
-        location.reload();
     });
 }
 main();
--- rps/src/main.ts
+++ rps/src/main.ts
@@ -1,6 +1,5 @@
 import {
   choices,
-  // choiceToEmoji,
   emojiMapping,
   EmojiOptions,
   evaluateGame,
@@ -24,99 +23,103 @@ function getElementByIdOrThrow<T extends HTMLElement = HTMLElement>(
   return element as T;
 }
 
+const comChoice = getElementByIdOrThrow<HTMLSpanElement>("com-choice");
+
+const round1 = getElementByIdOrThrow<HTMLParagraphElement>("round1");
+const round2 = getElementByIdOrThrow<HTMLParagraphElement>("round2");
+const round3 = getElementByIdOrThrow<HTMLParagraphElement>("round3");
+
+const tieBreaker = getElementByIdOrThrow<HTMLParagraphElement>("tie-breaker");
+
+const match = getElementByIdOrThrow<HTMLParagraphElement>("match-result");
+
+const resetbtn = getElementByIdOrThrow<HTMLButtonElement>("reset");
+resetbtn.disabled = true;
+resetbtn.addEventListener("click", () => {
+  location.reload();
+})
+
+function btns(): HTMLButtonElement[] {
+  let buttons: HTMLButtonElement[] = [];
+  for (const prs of choices) {
+    buttons.push(
+      getElementByIdOrThrow<HTMLButtonElement>(`btn-${prs}`),
+    );
+  }
+  return buttons;
+};
+
+const btnsArr: HTMLButtonElement[] = btns();
+
+function disableBtns(buttons: HTMLButtonElement[]) {
+  for (const btn of buttons) {
+    btn.disabled = true;
+  }
+}
+
+//*------------------------------------------------------------------------------------------------*/
+
+let counter: number = 1;
+let roundResultsArr: EmojiOptions[] = [];
+
+function displayRoundResults(verdict: Verdict) {
+  const elements: Record<number, HTMLElement> = {
+    1: round1,
+    2: round2,
+    3: round3,
+    4: tieBreaker,
+  };
+
+  if (counter in elements) { // if "counter" is a key in the "elements" object...
+    const element = elements[counter]!;
+    let result = emojiMapping[verdict];
+    if (counter <= 3) {
+      element.textContent = `Round ${counter}: ${result}`;
+      roundResultsArr.push(result);
+    } else {
+      tieBreaker.textContent = `Tie Breaker: ${result}`;
+      roundResultsArr.push(result);
+    }
+
+  }
+  counter++;
+}
+
+function evaluateOverallWinner() {
+  const counts = new Map<EmojiOptions, number>();
+
+  for (const value of roundResultsArr) {
+    counts.set(value, (counts.get(value) ?? 0) + 1);
+    counts.delete(emojiMapping.Tie);
+  }
+
+  for (const [emoji, count] of counts.entries()) {
+    // count does not need to diable buttons if verdict is tie.
+    if (count > 1) {
+      match.textContent = `Match: ${emoji}`;
+      disableBtns(btnsArr);
+      resetbtn.disabled = false;
+      break;
+    }
+  }
+}
+
+function forceNoTie(cc: PRS): PRS {
+  let tempChoices = choices.filter((value) => value !== cc);
+  let newCC = getRandomElement(tempChoices);
+  return newCC;
+}
+
 //*------------------------------------------------------------------------------------------------*/
 
 function main() {
-  const resultElm = getElementByIdOrThrow<HTMLDivElement>("result");
-  const comChoice = getElementByIdOrThrow<HTMLSpanElement>("com-choice");
 
-  const round1 = getElementByIdOrThrow<HTMLParagraphElement>("round1");
-  const round2 = getElementByIdOrThrow<HTMLParagraphElement>("round2");
-  const round3 = getElementByIdOrThrow<HTMLParagraphElement>("round3");
-
-  const tieBreaker = getElementByIdOrThrow<HTMLParagraphElement>("tie-breaker");
-
-  const match = getElementByIdOrThrow<HTMLParagraphElement>("match-result");
-
-  const resetbtn = getElementByIdOrThrow<HTMLButtonElement>("reset");
-  resetbtn.disabled = true;
-
-  const btns = function (): HTMLButtonElement[] {
-    let buttons: HTMLButtonElement[] = [];
-    for (const prs of choices) {
-      buttons.push(
-        getElementByIdOrThrow<HTMLButtonElement>(`btn-${prs}`),
-      );
-    }
-    return buttons;
-  };
-
-  function disableBtns() {
-    for (const btn of btns()) {
-      btn.disabled = true;
-    }
-  }
-
-  //*------------------------------------------------------------------------------------------------*/
-
-  let counter: number = 1;
-  let roundResultsArr: EmojiOptions[] = [];
-
-  function displayRoundResults(verdict: Verdict, uc: PRS, cc: PRS) {
-    const elements: Record<number, HTMLElement> = {
-      1: round1,
-      2: round2,
-      3: round3,
-      4: tieBreaker,
-    };
-
-    if (counter in elements) { // if "counter" is a key in the "elements" object...
-      const element = elements[counter]!;
-      let result = emojiMapping[verdict];
-      if (counter <= 3) {
-        element.textContent = `Round ${counter}: ${result}`;
-        roundResultsArr.push(result);
-      } else {
-        tieBreaker.textContent = `Tie Breaker: ${result}`;
-        roundResultsArr.push(result);
-      }
-
-    }
-    counter++;
-  }
-
-  function evaluateOverallWinner() {
-    const counts = new Map<EmojiOptions, number>();
-
-    for (const value of roundResultsArr) {
-      counts.set(value, (counts.get(value) ?? 0) + 1);
-      counts.delete(emojiMapping.Tie);
-    }
-
-    for (const [emoji, count] of counts.entries()) {
-      // count does not need to diable buttons if verdict is tie.
-      if (count > 1) {
-        match.textContent = `Match: ${emoji}`;
-        disableBtns();
-        resetbtn.disabled = false;
-        break;
-      }
-    }
-  }
-
-  function forceNoTie(cc: PRS): PRS {
-    let tempChoices = choices.filter((value) => value !== cc);
-    let newCC = getRandomElement(tempChoices);
-    return newCC;
-  }
-
-  //*------------------------------------------------------------------------------------------------*/
   let tieArr: Verdict[] = [];
 
   btns().forEach((btn) => {
     btn.addEventListener("click", (ev) => {
       console.log("Second", tieArr);
-      const uc = getComputerChoice();
+      const uc = btn.getAttribute("data-choice");
       let cc = getComputerChoice();
 
       assert(
@@ -135,17 +138,11 @@ function main() {
         }
       }
 
-      displayRoundResults(verdict, uc, cc);
+      displayRoundResults(verdict);
       evaluateOverallWinner();
 
       comChoice.textContent = emojiMapping[cc];
-      resultElm.textContent = `User: ${emojiMapping[verdict]}`;
     });
   });
-
-  resetbtn.addEventListener("click", (btn) => {
-    location.reload();
-  })
-
 }
 main();
